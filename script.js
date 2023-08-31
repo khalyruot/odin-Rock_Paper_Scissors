5a26741d29446a4d99ccb558b324cdb5924793e2
khaly <khalyruot12345@gmail.com>
2023-08-31 02:38:53 -0700

    update button

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
 console.log('Hello World!');
 function getComputerChoice(){
   const random_computer = Math.random();
+  if(random_compute == 1){
+    
+  }
   console.log("random_computer" + random_computer);
 }
 
 function getMyChoice(){
-  
+  let a = "rock";
+  let b = "paper";
+  let c = "scisor";
+
 }
 
 
