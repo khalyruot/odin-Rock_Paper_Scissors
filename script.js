1ed7c63ba74d038d083ed7260d7899c8a75f35c9
khaly <khalyruot12345@gmail.com>
2023-08-31 13:40:05 -0700

    add eventlistener to button

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
-console.log('Hello World!');
+const rock_button = document.getElementById("rock_button");
+const paper_button = document.getElementById("paper_button");
+const ciscor_button = document.getElementById("ciscor_button");
+
+rock_button.addEventListener("click", function(){
+  console.log("Rock");
+});
+
+paper_button.addEventListener("click", function(){
+  console.log("paper");
+});
+
+ciscor_button.addEventListener("click", function(){
+  console.log(ciscor);
+});
+
 function getComputerChoice(){
   const random_computer = Math.random();
   if(random_compute == 1){
