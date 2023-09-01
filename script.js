a3c7f569a5fdea18573c268365ff46e34582e4c6
khaly <khalyruot12345@gmail.com>
2023-09-01 13:30:49 -0700

    work on the login

--- script.js
+++ script.js
@@ -1,41 +1,38 @@
 const rock_button = document.getElementById("rock_button");
 const paper_button = document.getElementById("paper_button");
 const ciscor_button = document.getElementById("ciscor_button");
+let my_choice = '';
+const rock = 0;
+const paper = 1;
+const ciscor = 2;
+
+function getComputerChoice(){
+  const random_computer =  Math.floor(Math.random() * 3);
+  console.log("Computer_Random_Number" + random_computer);
+  
+}
+
 
 rock_button.addEventListener("click", function(){
-  alert("Rock");
+  my_choice = 1;
+  if(my_choice === random_computer){
+    alert(" You Draw");
+  }
+  
 });
 
 paper_button.addEventListener("click", function(){
-  alert("paper");
+  my_choice = 2;
+  alert("You chose Paper" + my_choice);
 });
 
 ciscor_button.addEventListener("click", function(){
-  alert("ciscor");
+  my_choice = 3;
+  alert("You chose Ciscor" +  my_choice);
 });
 
-function getComputerChoice(){
-  const random_computer = Math.random();
-  if(random_compute == 1){
-    
-  }
-  console.log("random_computer" + random_computer);
-}
 
-function getMyChoice(){
-  let a = "rock";
-  let b = "paper";
-  let c = "scisor";
-
-}
+console.log("my_choice: " + my_choice);
 
 
-function playRound(playerSelection, computerSelection) {
-  // your code here!
-}
-
-
-const playerSelection = "rock";
-const computerSelection = getComputerChoice();
-console.log(playRound(playerSelection, computerSelection));
 
