36427e465ad027cabd713e8473824ff8e78da7be
khaly <khalyruot12345@gmail.com>
2023-09-03 00:53:22 -0700

    work on logic

--- script.js
+++ script.js
@@ -1,38 +1,55 @@
 const rock_button = document.getElementById("rock_button");
 const paper_button = document.getElementById("paper_button");
 const ciscor_button = document.getElementById("ciscor_button");
-let my_choice = '';
-const rock = 0;
-const paper = 1;
-const ciscor = 2;
+
+const random_computer =  Math.floor(Math.random() * 3);
 
 function getComputerChoice(){
-  const random_computer =  Math.floor(Math.random() * 3);
   console.log("Computer_Random_Number" + random_computer);
   
 }
 
 
 rock_button.addEventListener("click", function(){
-  my_choice = 1;
-  if(my_choice === random_computer){
-    alert(" You Draw");
+  if(random_computer == 2){
+    alert("You lost! And the computer chose Paper.");
+    getComputerChoice();
+  }
+  else if(random_computer == 3){
+    alert("You win! And the computer chose Ciscor.");
+  }
+  else{
+    alert("You draw! And the computer chose Rock.");
+  }
+   
+});
+
+paper_button.addEventListener("click", function(){
+  if(random_computer == 1){
+    alert("You Win! And the computer chose Rock.");
+  }
+  else if(random_computer == 3){
+    alert("You lose! And the computer chose Ciscor.");
+  }
+  else{
+    alert("You draw! And the computer chose Paper.");
   }
   
 });
 
-paper_button.addEventListener("click", function(){
-  my_choice = 2;
-  alert("You chose Paper" + my_choice);
-});
-
 ciscor_button.addEventListener("click", function(){
-  my_choice = 3;
-  alert("You chose Ciscor" +  my_choice);
+  if(random_computer == 1){
+    alert("You lose! And the computer chose Rock.");
+  }
+  else if(random_computer == 2){
+    alert("You Win! And the computer chose Paper.");
+  }
+  else{
+    alert("You draw! And the computer chose Ciscor.");
+  }
+  
 });
 
-
-console.log("my_choice: " + my_choice);
 
 
 
