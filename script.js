a504e24782f39be6a507bb56be631a9d7b820b19
khaly <khalyruot12345@gmail.com>
2023-09-03 12:35:28 -0700

    create the pop-up text

--- script.js
+++ script.js
@@ -4,48 +4,43 @@ const ciscor_button = document.getElementById("ciscor_button");
 
 const random_computer =  Math.floor(Math.random() * 3);
 
-function getComputerChoice(){
-  console.log("Computer_Random_Number" + random_computer);
-  
-}
 
 
 rock_button.addEventListener("click", function(){
   if(random_computer == 2){
-    alert("You lost! And the computer chose Paper.");
-    getComputerChoice();
+    document.getElementById("demo").innerHTML = "You lost! And the computer chose Paper.";
   }
   else if(random_computer == 3){
-    alert("You win! And the computer chose Ciscor.");
+    document.getElementById("demo").innerHTML = "You win! And the computer chose Ciscor.";
   }
   else{
-    alert("You draw! And the computer chose Rock.");
+    document.getElementById("demo").innerHTML = "You draw! And the computer chose Rock.";
   }
    
 });
 
 paper_button.addEventListener("click", function(){
   if(random_computer == 1){
-    alert("You Win! And the computer chose Rock.");
+    document.getElementById("demo").innerHTML = "You Win! And the computer chose Rock.";
   }
   else if(random_computer == 3){
-    alert("You lose! And the computer chose Ciscor.");
+    document.getElementById("demo").innerHTML = "You lose! And the computer chose Ciscor.";
   }
   else{
-    alert("You draw! And the computer chose Paper.");
+    document.getElementById("demo").innerHTML = "You draw! And the computer chose Paper.";
   }
   
 });
 
 ciscor_button.addEventListener("click", function(){
   if(random_computer == 1){
-    alert("You lose! And the computer chose Rock.");
+    document.getElementById("demo").innerHTML = "You lose! And the computer chose Rock.";
   }
   else if(random_computer == 2){
-    alert("You Win! And the computer chose Paper.");
+    document.getElementById("demo").innerHTML = "You Win! And the computer chose Paper.";
   }
   else{
-    alert("You draw! And the computer chose Ciscor.");
+    document.getElementById("demo").innerHTML = "You draw! And the computer chose Ciscor.";
   }
   
 });
